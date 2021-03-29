53bc43c0ef30f4f496348558ad7d1d5c8f357ae6
oozry12 <oozryr@gmail.com>
2021-03-29 11:47:25 +0800

    Update and rename _app.js to _app.jsx

---
+++ pages/_app.jsx
@@ -0,0 +1,5 @@
+import "react-notion/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
